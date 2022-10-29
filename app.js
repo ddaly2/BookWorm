1b25ae263e28e5a4cc9d29bb4830bebf3f8b6716
ddaly2 <104313049+ddaly2@users.noreply.github.com>
2022-10-29 11:18:41 -0500

    Moved JS to index.html
    Now runs on Node server but scripts are executed client side.

--- app.js
+++ app.js
@@ -1,31 +1,13 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import ejs from "ejs";
-import _ from "lodash";
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.use("/public", express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-     res.render("home");
+   
 })
-
-app.post("/", (req, res) => {
-     console.log(req.body.text);
-})
-
-
-
-
-
-
 
 app.listen(3000, () => {
      console.log("Server started on port 3000");
-})
+});
