071227ab60cb77bdc6028fad649750744fc9d731
ddaly2 <104313049+ddaly2@users.noreply.github.com>
2022-11-01 16:22:05 -0500

    Successfully added filter for ratings from high --> low

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let searchInfo = "";
 let searched = false;
 let booksArr = [];
 
+
 async function getBooks() {
      const url = `https://www.googleapis.com/books/v1/volumes?q=${searchInfo}`;
      const response = await fetch(url);
@@ -55,14 +56,13 @@ async function getBooks() {
                document.getElementById(`bookCard${i}`).innerHTML += `${book.publishedDate}`;
           }
 
-          
           // Check for thumbnail image link
           if(data.items[i].volumeInfo.hasOwnProperty("imageLinks")) {
                book.thumbnail = data.items[i].volumeInfo.imageLinks.thumbnail;
                document.getElementById(`bookCard${i}`).innerHTML += `<img src=${book.thumbnail} />`;
           } else {
-               book.thumbnail = "<p>No cover available</p>";
-               document.getElementById(`bookCard${i}`).innerHTML += `${book.thumbnail}`;
+               book.thumbnail = undefined;
+               document.getElementById(`bookCard${i}`).innerHTML += `<p>No cover available</p>`;
           }
 
           // Add new book object
@@ -80,44 +80,66 @@ document.getElementById("searchForm").addEventListener("submit", (e) => {
           getBooks();
           document.getElementById("searchMessage").innerHTML = `Showing results for: "${searchInfo}"`;
           searched = true;
-          console.log(searchInfo);
      } else {
           e.preventDefault();
           getSearch(e.target);
           getBooks();
           document.getElementById("books").innerHTML = "";
           document.getElementById("searchMessage").innerHTML = `Showing results for: "${searchInfo}"`;
-          console.log(searchInfo);
      }
 })
 
 //Add filter options to the booksArr
 document.getElementById("filter").addEventListener("change", (e) => {
      let filterChoice = e.target.value;
+     let ratedHighArr = [];
      console.log(filterChoice);
      if(filterChoice === "ratedHigh") {
-          booksArr.sort((a, b) => {
+
+          ratedHighArr = [...booksArr];
+          let hasNoRatingArr = [];
+          let hasRatingArr = [];
+          for (let i = 0; i < ratedHighArr.length; i++) {
+               if (ratedHighArr[i].averageRating === undefined){
+                    hasNoRatingArr.push(ratedHighArr[i]);
+               } else {
+                    hasRatingArr.push(ratedHighArr[i]);
+               }
+          }
+          
+          hasRatingArr.sort((a, b) => {
                return b.averageRating - a.averageRating;
           });
+
+          ratedHighArr = [...hasRatingArr, ...hasNoRatingArr];
           
-          for (let i = 0; i < booksArr.length; i++) {
-               console.log(booksArr[i].averageRating);
-               document.getElementById(`bookCard${i}`).innerHTML = `<h2>${booksArr[i].title}</h2>`;
+          for (let i = 0; i < ratedHighArr.length; i++) {
+               console.log(ratedHighArr[i].averageRating);
+               document.getElementById(`bookCard${i}`).innerHTML = `<h2>${ratedHighArr[i].title}</h2>`;
                //Double check if the item has a rating
-               if (booksArr[i].averageRating !== undefined) {
-                    document.getElementById(`bookCard${i}`).innerHTML += `<p>Rating: ${booksArr[i].averageRating}⭐️</p>`;
+               if (ratedHighArr[i].averageRating !== undefined) {
+                    document.getElementById(`bookCard${i}`).innerHTML += `<p>Rating: ${ratedHighArr[i].averageRating}⭐️</p>`;
                } else {
                     document.getElementById(`bookCard${i}`).innerHTML += `<p>Rating: n/a</p>`;
                }
                //Double check for price info
-               if (booksArr[i].price !== undefined) {
-                    document.getElementById(`bookCard${i}`).innerHTML += `<p>Price: $${booksArr[i].price}</p>`;
+               if (ratedHighArr[i].price !== undefined) {
+                    document.getElementById(`bookCard${i}`).innerHTML += `<p>Price: $${ratedHighArr[i].price}</p>`;
                } else {
                     document.getElementById(`bookCard${i}`).innerHTML += `<p>Price: n/a</p>`;
                }
-               
-               document.getElementById(`bookCard${i}`).innerHTML += `<p>Published: ${booksArr[i].publishedDate}</p>`;
-               document.getElementById(`bookCard${i}`).innerHTML += `<img src=${booksArr[i].thumbnail} />`;
+               //Double check for publishedDate
+               if (ratedHighArr[i].publishedDate !== undefined) {
+                    document.getElementById(`bookCard${i}`).innerHTML += `<p>Published: ${ratedHighArr[i].publishedDate}</p>`;
+               } else {
+                    document.getElementById(`bookCard${i}`).innerHTML += `<p>Published: n/a</p>`;
+               }
+               //Double check for thumbnail 
+               if (ratedHighArr[i].thumbnail !== undefined) {
+                    document.getElementById(`bookCard${i}`).innerHTML += `<img src=${ratedHighArr[i].thumbnail}>`;
+               } else {
+                    document.getElementById(`bookCard${i}`).innerHTML += `<p>No cover available</p>`;
+               }
           }
      }
      
